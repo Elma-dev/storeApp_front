db2604981e3b76c798a197767f2ebb4ed1779398
Elma-div <elmajjodi.abdeljalil@gmail.com>
2023-05-08 20:47:39 +0100

    commit

--- src/app/components/inventory/inventory.component.ts
+++ src/app/components/inventory/inventory.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {InventoryService} from "../../services/inventory.service";
 
 @Component({
   selector: 'app-inventory',
   templateUrl: './inventory.component.html',
   styleUrls: ['./inventory.component.css']
 })
-export class InventoryComponent {
+export class InventoryComponent implements OnInit{
+
+  products:any;
+  error!: string;
+  constructor(private inventoryService:InventoryService) {
+  }
+  ngOnInit(): void {
+    this.inventoryService.getAllProducts().subscribe({
+      next:(data)=>{
+        this.products=data;
+      },
+      error:(erros)=>{
+        this.error=erros;
+      }
+    })
+  }
 
 }
--- src/app/services/inventory.service.ts
+++ src/app/services/inventory.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class InventoryService {
 
-  constructor() { }
+  constructor(private httpClient:HttpClient) { }
+
+  public getAllProducts():Observable<any>{
+    return this.httpClient.get("http://localhost:8888/INVENTORY-SERVICE/products");
+  }
 }
