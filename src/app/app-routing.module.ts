b9795b41562ab3f3b3b638fdd7d6d50197cb89ed
Elma-div <elmajjodi.abdeljalil@gmail.com>
2023-05-08 17:22:21 +0100

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CustomerComponent} from "./components/customer/customer.component";
+import {BillingComponent} from "./components/billing/billing.component";
+import {InventoryComponent} from "./components/inventory/inventory.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'customers',component:CustomerComponent},
+  {path:'billings',component:BillingComponent},
+  {path:'inventory',component:InventoryComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
